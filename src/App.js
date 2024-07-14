775095c41eb04b4ff83e5eee48a8a675d7de5cb3
rnj10 <rnj1010@gmail.com>
2024-07-14 23:03:10 +0900

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './css/App.css';
+import './css/fileupload.css';
+import './css/progressbar.css';
+import './css/extractimg.css';
+import './css/dataanalysis.css';
+import './css/chat.css';
+
+import { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Route, Routes, useNavigate, useLocation} from 'react-router-dom';
+
+
+import UploadPage from './pages/UploadPage';
+import ExtractImage from './pages/ExtractImage';
+import DataAnalysis from './pages/DataAnalysis';
+import ProgressBar from './components/ProgressBar';
 
 function App() {
+  const [step, setStep] = useState(1);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const handleNextStep = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+  useEffect(()=>{
+    navigate('/');
+  },[]);
+  useEffect(() => {
+    // 초기 페이지 로드 시 위치에 따라 step 설정
+    switch (location.pathname) {
+      case '/':
+        setStep(1);
+        break;
+      case '/step2':
+        setStep(2);
+        break;
+      case '/step3':
+        setStep(3);
+          break;
+      default:
+        // 기본적으로 첫 페이지로 리다이렉트
+        navigate('/');
+    }
+  }, [location.pathname]); 
+
+  useEffect(() => {
+    // step 변경 시 라우트 이동
+    switch (step) {
+      case 1:
+        if (location.pathname !== '/') navigate('/');
+        break;
+      case 2:
+        if (location.pathname !== '/step2') navigate('/step2');
+        break;
+      case 3:
+        if (location.pathname !== '/step3') navigate('/step3');
+        break;
+    }
+  }, [step]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className='logo-container'>
+            <p className='logo-corning'>Corning</p>
+            <div className='logo-charteye'>
+              <p>ChartEye</p>
+              <img src={'resources/icons/icon.svg'} alt="icon"/>
+            </div>
+          </div>
+        <div className='main-content'>
+          <div className='progress-container'>
+            <ProgressBar step={step}/>
+          </div>
+          <Routes>
+            <Route exact path="/" element={<UploadPage onNext={handleNextStep}/>}/>
+            <Route path="/step2" element={<ExtractImage onNext={handleNextStep}/>}/>
+            <Route path ="/step3" element={<DataAnalysis/>}/>
+          </Routes>
+        </div>
+      </div>
   );
 }
 
