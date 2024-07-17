1611c6bdb240c03775aa748a104fa0ea2111b629
Tro-fish <waniboyy@gmail.com>
2024-07-17 18:25:15 +0900

    modify inferene_test_code

--- models/PaliGemma/inference_for_test.py
+++ models/PaliGemma/inference_for_test.py
@@ -11,15 +11,15 @@ device = "cuda:1"
 prompt = "caption en\n"
 dtype = torch.bfloat16
 image_path_prefix = "/home/wani/Desktop/Corning_team3/dataset/scicap_data/dataset/val/images/"
-checkpoint_path = '/home/wani/Desktop/Corning_team3/models/PaliGemma/train/checkpoints/'
+
 with open ("/home/wani/Desktop/Corning_team3/dataset/scicap_data/dataset/val/final_validation_v2.json", 'r') as f:
     validation_data = json.load(f)
 
-# 모델을 체크포인트에서 불러옴
 model = PaliGemmaForConditionalGeneration.from_pretrained(
-    checkpoint_path,
+    model_id,
     torch_dtype=dtype,
     device_map=device,
+    revision="bfloat16",
 ).eval()
 
 processor = AutoProcessor.from_pretrained(model_id)
