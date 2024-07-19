0e0eaf39b1df86bf73e9d2561a60ce6c2e39abd3
hwany99 <105199942+hwany99@users.noreply.github.com>
2024-07-19 20:32:53 +0900

    Update get_answer

--- web/backend/chat/views.py
+++ web/backend/chat/views.py
@@ -29,28 +29,77 @@ def chat(request):
         return JsonResponse({'status': 'success', 'message': 'Files received and processed', 'answer': answer})
     return JsonResponse({'status': 'fail', 'message': 'Invalid request method'}, status=405)
 
-def get_answer(image, text, caption):
-    global client
-
-    prompt = f'''[Caption of Figure] {caption}
-
-[Question] {text}'''
+#gpt4o
+def get_answer(figure_path, user_prompt, caption="", ocr_text=""):
+    global client 
+    with open(figure_path, "rb") as image_file:
+        encoded_figure = base64.b64encode(image_file.read()).decode('utf-8')
 
     response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": "Please answer the question with reference to the image."},
-            {"role": "user", "content": [
-                {"type": "text", "text": prompt},
-                {"type": "image_url", "image_url": {
-                    "url": image}
-                }
-            ]}
+            model="gpt-4o",
+            temperature=0.0,
+            messages= [
+            {
+                "role": "user", "content": [
+                    {
+                        "type": "image_url", 
+                        "image_url": {
+                            "url": f"data:image/png;base64,{encoded_figure}",
+                        }
+                    },
+                    {
+                        "type": "text", 
+                        "text": f"""
+                            <Information1>:
+                            "If there are multiple charts, please process each chart individually first.
+
+                            Instructions for the whole chart:
+                            1. Extract the title of the chart and all text around the chart image. Record the position of each text to refer to it later.
+                            2. Check the legend and if there are colors, record what data each color represents. Ensure to accurately record the position and content of the legend to match the colors within the chart.
+                                    
+                            Instruction for bar chart:
+                            1. Identify and record the labels of the x-axis and y-axis. For vertical bar charts, the x-axis is usually at the bottom and the y-axis is on the left. For horizontal bar charts, the x-axis is on the left and the y-axis can be on the right or left.
+                            2. If there are annotations on the bars showing text values, record these values. If annotations provide explanations, record the exact position of the bar they refer to.
+                            3. If there are no annotations, estimate the values by considering the values and range of the y-axis.
+
+                            Instruction for line chart: 
+                            1. Identify and record the labels of the x-axis and y-axis. The x-axis is usually at the bottom, and the y-axis is on the left.
+                            2. If there are annotations near the points on the line showing text values, record these values. If annotations provide explanations, record the exact position of the point they refer to.
+
+
+                            Instruction for pie chart: 
+                            1. Identify and record the color of each pie section along with the corresponding text. Ensure to refer to this information later.
+                            2. If there are annotations near the sections showing text values, record these values. If annotations provide explanations, record the exact position of the pie section they refer to.
+                            
+                            Other scatter plots, box plots, etc:
+                            Extract data using similar methods as the above charts. Identify the unique features of each chart and extract data accordingly."
+
+                            <Information2>:
+                            {caption}
+                            {ocr_text}
+                            
+                            
+                            <Question Case>:
+                            "case1. If you need to estimate the value for a specific position, carefully consider the range of values on the y-axis or x-axis, the minor tick marks, and how close the value is to other known values to make a reasonable estimation.
+                            case2. If the highest or lowest value in the graph is needed, estimate where that value might be located based on the graph's data distribution.
+                            case3. For characteristics or information about the graph, extract the text and use it to infer the details."
+                            
+
+                            Using the <Information1> and <Information2> methods provided, for the given image
+                            "<User Question>:
+                            {user_prompt}"
+                            
+                            Select the appropriate case from the <Question Case> and provide an answer to the <User Question> based on it.
+                            
+                            <Answer>:
+                            
+                            """
+                    },
+                ]
+            },
         ]
     )
-
-    answer = response.choices[0].message.content
-    return answer
+    return response.choices[0].message.content
 
 def get_answer_llava(img_str, text, caption):
     global device, model, processor
