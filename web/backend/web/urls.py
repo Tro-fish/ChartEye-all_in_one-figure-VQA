a0e011e2fbf17584eacaac0f4cda70c8c71b9fde
wjieun <wjieun@g.skku.edu>
2024-07-27 20:12:14 +0900

    public

--- web/backend/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('extract/', include('extract_images.urls')),
-    path('caption/', include('caption.urls')),
-    path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
-]
